b7ecb589d557d79e08b41cb08ae6b7dc368163d7
Sebastian Espinoza <sespinozar25@ilb.cat>
2026-03-25 10:12:47 +0100

    Estructura inicial y addición de los elementos del DOM

---
+++ script.js
@@ -0,0 +1,11 @@
+let alumnes = [];
+
+const formulari = document.getElementById("formulariAlumne");
+const nomInput = document.getElementById("nom");
+const examenInput = document.getElementById("examen");
+const practiquesInput = document.getElementById("practiques");
+const actitudInput = document.getElementById("actitud");
+const missatge = document.getElementById("missatge");
+const cosTaula = document.getElementById("cosTaula");
+const btnAsc = document.getElementById("ordenarAsc");
+const btnDesc = document.getElementById("ordenarDesc");
